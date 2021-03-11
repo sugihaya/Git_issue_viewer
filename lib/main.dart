154a8257bf086613865f69a1a7ce6de8e9be5efe
Hayato Sugimoto <10haya22ton@gmail.com>
2021-03-11 18:47:14 +0900

    TabBarの作成

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,52 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  // Tabウィジェットのリスト
+  final List<Tab> tabs = <Tab>[
+    Tab(text: '全て'),
+    Tab(text: 'p: webview'),
+    Tab(text: 'p: shared_preferences'),
+    Tab(text: 'waiting for customer response'),
+    Tab(text: 'severe: new feature'),
+    Tab(text: 'p: share'),
+  ];
+
+  TabController _tabController;
+
+  // TabControllerの初期化
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+          title: Text(widget.title),
+          bottom: TabBar(
+            tabs: tabs,
+            controller: _tabController,
+            isScrollable: true,
+            unselectedLabelStyle: TextStyle(fontSize: 12.0),
+            labelStyle: TextStyle(fontSize: 16.0),
+            indicatorWeight: 2,
+          )),
+      body: TabBarView(
+        controller: _tabController,
+        children: tabs.map((tab) {
+          return _createTab(tab);
+        }).toList(),
       ),
-      body: Center(),
+    );
+  }
+
+  Widget _createTab(Tab tab) {
+    return Center(
+      child: Text('test'),
     );
   }
 }
